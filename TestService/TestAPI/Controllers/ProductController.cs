898f6468cde35d766449255d26ec94497532e542
Nirav Sanghadiya <nirav.sanghadiya8@gmail.com>
2023-03-04 23:20:12 +0530

    File changes added

---
+++ TestService/TestAPI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestAPI.Data;
+using TestAPI.Models;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.AsEnumerable();
+        }
+    }
+}
---
+++ TestService/TestAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TestAPI.Models;
+
+namespace TestAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
